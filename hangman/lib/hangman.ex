0058cd4e930c062e6a58cf49295460fc69b98037
firerobin <30886619+firerobin@users.noreply.github.com>
2017-10-30 00:23:22 -0500

    feature-1

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -11,7 +11,7 @@ defmodule Hangman do
 
   def new_game() do
     word = Dictionary.random_word()
-    word = Regex.split(~r{}, word, trim: true) |> List.delete(word, "\r")
+    word = Regex.split(~r{}, word, trim: true) |> List.delete("\r")
     letters = Enum.reduce(word, [], fn (_, acc) -> ["_" | acc] end)
     %Hangman{word: word, letters: letters}
   end
